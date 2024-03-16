c0c886eaffcbb2b14d4b7a9d07d0105f58f005e5
Samu Cabrera <samucabreram99@gmail.com>
2024-03-16 11:43:12 -0300

    Funcionalida de navegacion del menu lateral

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,33 +1,34 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
 
   //vamos a utilizar directamente el objeto de las rutas
- /*  public menuItems = routes
-  .map(route => route.children)
-  .flat() //convierte en un arreglo plano
-  .filter(route => route && route?.path)
-  .filter(route => !route?.path?.includes(':')); */
 
-  public menuItems = [
-    {
-      title: 'User',
-      path: 'user/:id'
-    },
-    {
-      title: 'Lista de usuarios',
-      path: 'users'
-    }
-  ]
+  public menuItems = routes
+  .map(route => route.children || [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(router => !router.path?.includes(':'));
+
+  constructor(){
+    /* const menuItems = routes
+    .map(route => route.children || [])
+    .flat() //convierte el arreglo en plano, es decir, que tenemos un array dentro de otra, esta funcion lo convierte en una sola
+    .filter(route => route && route.path)
+    .filter(router => !router.path?.includes(':'))
+
+    console.log(menuItems); */
+  }
 
 
 }
